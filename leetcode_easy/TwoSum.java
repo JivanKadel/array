490d5b3e25f4cde926101c39ca3d87319eb2ad67
Jivan Kadel <jivankadel256@gmail.com>
2025-01-21 12:11:46 +0545

    Two sum problem using HashMap

--- leetcode_easy/TwoSum.java
+++ leetcode_easy/TwoSum.java
@@ -1,7 +1,36 @@
 package leetcode_easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public static int[] returnSumIndices(int[] numbers, int sum){
+        Map<Integer, Integer> valueIndexMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            valueIndexMap.put(numbers[i], i);
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int addend = sum - numbers[i];
+            if(valueIndexMap.containsKey(addend) && ( valueIndexMap.get(addend) != i)){
+                return new int[] {i, valueIndexMap.get(addend)};
+            }
+        }
+        return null;
+    }
+    public static void printArray(int[] numbers){
+        if(numbers != null){
+            for(int num: numbers){
+                System.out.print(num + "  ");
+            }
+            System.out.println();
+        }else{
+            System.out.println("The array is null");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Yo");
+        printArray(returnSumIndices(new int[] {1, 2, 3, 5, 6}, 6));
+        printArray(returnSumIndices(new int[] {1, 2, 3, 2, -1}, 4));
+        printArray(returnSumIndices(new int[] {1, 2, 3, 2, -1}, 1));
     }
 }
